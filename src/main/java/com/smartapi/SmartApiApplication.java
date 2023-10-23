6dfbc04fa5f07f55290d0bc388913ed72173520d
Vijay <vijaykumarvijay886@gmail.com>
2023-10-23 15:33:45 +0530

    update trade 1

--- src/main/java/com/smartapi/SmartApiApplication.java
+++ src/main/java/com/smartapi/SmartApiApplication.java
@@ -36,7 +36,7 @@ public class SmartApiApplication {
 
 	public static void main(String[] args) throws UnknownHostException {
 		context = SpringApplication.run(SmartApiApplication.class, args);
-		log.info("Started jar at address {}, Build {}", InetAddress.getLocalHost(), "21-10 1");
+		log.info("Started jar at address {}, Build {}", InetAddress.getLocalHost(), "23-10 1");
 	}
 
 	public static void restart() {
